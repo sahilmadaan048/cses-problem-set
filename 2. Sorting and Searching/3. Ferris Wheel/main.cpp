4b7dc3a90421364a005d6be1a7d023806e0d6f84
Sahil Madaan <madaan.sahil27@gmail.com>
2025-04-22 23:10:04 +0530

    ferris wheel

---
+++ 2. Sorting and Searching/3. Ferris Wheel/main.cpp
@@ -0,0 +1,84 @@
+// https://cses.fi/problemset/task/1090
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+
+void solve() {
+    int n, x; 
+    cin >> n >> x;
+    vector<int> p(n);
+    for (int i = 0; i < n; i++) cin >> p[i];
+
+    // Sort the weights in ascending order
+    sort(p.begin(), p.end());
+
+    int gondolas = 0;
+    int i = 0;  // Pointer to the lightest child
+    int j = n - 1;  // Pointer to the heaviest child
+    
+    // Loop to process all the children
+    while (i <= j) {
+        if (p[i] + p[j] <= x) {
+            // If the lightest and heaviest child can share a gondola
+            gondolas++;
+            i++;  // Move the lightest child
+            j--;  // Move the heaviest child
+        } else {
+            // If they can't share, the heaviest child goes alone
+            gondolas++;
+            j--;  // Move only the heaviest child
+        }
+    }
+
+    // Output the total number of gondolas
+    cout << gondolas << endl;
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}

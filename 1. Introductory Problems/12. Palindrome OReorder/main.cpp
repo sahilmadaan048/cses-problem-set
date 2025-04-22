55693ff9a2ce7ef084a5bf819cf2c0338af13cf6
sahilmadaan048 <madaan.sahil27@gmail.com>
2025-04-22 11:17:24 +0530

    palindrome reorder

---
+++ 1. Introductory Problems/12. Palindrome OReorder/main.cpp
@@ -0,0 +1,94 @@
+// https://cses.fi/problemset/task/1755
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+void solve() {
+    string s; cin >> s;
+    unordered_map<char, int> freq;
+    for (char c : s) freq[c]++;
+
+    int odd_count = 0;
+    char odd_char = 0;
+    //there should be only 1 character with an odd count
+    for (auto [ch, cnt] : freq) {
+        if (cnt % 2 != 0) {
+            odd_count++;
+            odd_char = ch;
+        }
+    }
+
+    // if more than 1 chars have count odd , no solution possuble
+    if (odd_count > 1) {
+        cout << "NO SOLUTION\n"; // or just print nothing
+        return;
+    }
+
+    string half = "";
+    for (auto& [ch, cnt] : freq) {
+        int repeat = cnt / 2; //always repeat the character n/2 since we are forming the first half of the string
+        half += string(repeat, ch);
+        
+        // we are done with this character
+        freq[ch] -= 2 * repeat;
+    }
+
+
+    //handling the character with an odd count
+    string mid = (odd_count == 1) ? string(1, odd_char) : "";
+
+    string rev_half = half;
+    //now rverse it
+    reverse(rev_half.begin(), rev_half.end());
+    cout << half << mid << rev_half << endl;
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    

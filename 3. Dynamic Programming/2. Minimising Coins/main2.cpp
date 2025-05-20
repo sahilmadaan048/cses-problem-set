9d6824f29317bbb277de5d7c5cd5ffbf7d12a051
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-20 09:29:29 +0530

    tabulation

---
+++ 3. Dynamic Programming/2. Minimising Coins/main2.cpp
@@ -0,0 +1,64 @@
+// https://cses.fi/problemset/task/1634
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll             long long int 
+#define ulli           unsigned long long int 
+#define li             long int 
+#define ff(i,a,b)      for(int i=a;i<b;i++)
+#define fb(i,b,a)      for(int i=b;i>=a;i--)
+#define w(t)           while(--t >= 0)
+#define l(s)           s.length()
+#define ci(n)          cin>>n;
+#define fast           ios_base::sync_with_stdio(false);
+#define sa(a,n)        sort(a,a+n)
+#define sv(v)          sort(v.begin(),v.end())
+#define cy             cout<<"YES\n"
+#define cn             cout<<"NO\n"
+#define nl             cout<<"\n"
+#define minus          cout<<"-1\n";
+#define vi             vector<int>
+#define pb             push_back
+#define tc             int t; cin>>t;
+#define pp             pair<int,int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+#define co(n)          cout<<n;
+#define ret            return 0
+#define mi             map<int,int>
+#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";   
+#define forn(i, n)     ff(i, 0, n)
+#define sz(v)          int((v).size())\
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> coins(n);
+    for (int i = 0; i < n; i++) cin >> coins[i];
+
+    vector<int> dp(x + 1, INT_MAX);
+    dp[0] = 0;
+
+    for (int i = 1; i <= x; i++) {
+        for (int c : coins) {
+            if (i - c >= 0 && dp[i - c] != INT_MAX) {
+                dp[i] = min(dp[i], dp[i - c] + 1);
+            }
+        }
+    }
+
+    if (dp[x] == INT_MAX) cout << -1 << "\n";
+    else cout << dp[x] << "\n";
+}
+
+int main(){
+	fast;
+	int t=1;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
+	

e99a6cff2188468d4732ce3543b52c3c2c348bc5
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-22 11:15:10 +0530

    space optimised

---
+++ 3. Dynamic Programming/7. Book Shop/main4.cpp
@@ -0,0 +1,61 @@
+// https://cses.fi/problemset/task/1158
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll             long long int 
+#define ulli           unsigned long long int 
+#define li             long int 
+#define ff(i,a,b)      for(int i=a;i<b;i++)
+#define fb(i,b,a)      for(int i=b;i>=a;i--)
+#define w(t)           while(--t >= 0)
+#define l(s)           s.length()
+#define ci(n)          cin>>n;
+#define fast           ios_base::sync_with_stdio(false);
+#define sa(a,n)        sort(a,a+n)
+#define sv(v)          sort(v.begin(),v.end())
+#define cy             cout<<"YES\n"
+#define cn             cout<<"NO\n"
+#define nl             cout<<"\n"
+#define minus          cout<<"-1\n";
+#define vi             vector<int>
+#define pb             push_back
+#define tc             int t; cin>>t;
+#define pp             pair<int,int>
+#define input(a,n)     for(int i=0;i<n;i++) cin>>a[i];
+#define mod            1000000007
+#define co(n)          cout<<n;
+#define ret            return 0
+#define mi             map<int,int>
+#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";   
+#define forn(i, n)     ff(i, 0, n)
+#define sz(v)          int((v).size())
+
+void solve(){
+	int n, x;
+	cin >> n >> x;
+	vector<int> h(n), s(n);
+	for (int i = 0; i < n; ++i) cin >> h[i];
+	for (int i = 0; i < n; ++i) cin >> s[i];
+
+	vector<int> dp(x + 1, 0);
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = x; j >= h[i]; --j) {
+			dp[j] = max(dp[j], dp[j - h[i]] + s[i]);
+		}
+	}
+
+	cout << dp[x] << endl;
+}
+
+
+int main(){
+	fast;
+	int t=1;
+	while(t--){
+		solve();
+	}
+	return 0;
+}

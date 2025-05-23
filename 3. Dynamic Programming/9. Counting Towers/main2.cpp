7475fccdd565e37d4b2acde2fdffe11b64a064fe
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-23 07:56:14 +0530

    tabulation

---
+++ 3. Dynamic Programming/9. Counting Towers/main2.cpp
@@ -0,0 +1,63 @@
+// cses.fi/problemset/task/2413/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+const int md = 1e9+7;
+const int mxN = 1e6+6;
+
+int dp[mxN][3]; // dp[i][1] = separate end, dp[i][2] = fused end
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    // Base cases
+    dp[1][1] = 1; // separate last blocks
+    dp[1][2] = 1; // fused last blocks
+
+    // Fill the table up to maximum possible n
+    for (int i = 2; i < mxN; i++) {
+        dp[i][1] = (dp[i-1][1] * 4 % md + dp[i-1][2]) % md;
+        dp[i][2] = (dp[i-1][1] + dp[i-1][2] * 2 % md) % md;
+    }
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << (dp[n][1] + dp[n][2]) % md << endl;
+    }
+}

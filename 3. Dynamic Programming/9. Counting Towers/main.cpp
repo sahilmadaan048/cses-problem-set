8c67bba54ae1fba04bf9b2df89f6718242387225
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-23 07:54:44 +0530

    memoised

---
+++ 3. Dynamic Programming/9. Counting Towers/main.cpp
@@ -0,0 +1,64 @@
+// cses.fi/problemset/task/2413/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+const int md = 1e9+7;
+const int mxN = 1e6+6;
+vector<vector<int>> memo(mxN, vector<int>(3, -1));
+
+
+// type = 1: separate last blocks
+// type = 2: fused last blocks
+int solve(int n, int type) {
+    if (n == 1) return 1;
+    if (memo[n][type] != -1) return memo[n][type];
+
+    if (type == 1) {
+        return memo[n][1] = (solve(n - 1, 1) * 4 % md + solve(n - 1, 2)) % md;
+    } else {
+        return memo[n][2] = (solve(n - 1, 1) + solve(n - 1, 2) * 2 % md) % md;
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        cout << (solve(n, 1) + solve(n, 2)) % md << endl;
+    }
+}

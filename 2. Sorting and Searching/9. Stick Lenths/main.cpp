49e73e4564de2b2bff41d487a3d9ecb7008710d7
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-01 14:28:51 +0530

    Stick lengths

---
+++ 2. Sorting and Searching/9. Stick Lenths/main.cpp
@@ -0,0 +1,25 @@
+// https://cses.fi/problemset/task/1074
+ 
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+#define vi vector<int>
+#define fast ios_base::sync_with_stdio(false); cin.tie(0);
+ 
+void solve(){
+    int n; cin >> n;
+    vi temp(n);
+    for(int i = 0; i < n; i++) cin >> temp[i];
+    sort(temp.begin(), temp.end());
+    int median = temp[n / 2];
+    ll cnt = 0;
+    for(int i = 0; i < n; i++) cnt += abs(temp[i] - median);
+    cout << cnt << "\n";
+}
+ 
+int main(){
+    fast;
+    solve();
+    return 0;
+}

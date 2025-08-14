24db391f91dcf40b6ce5512315ca4bc18fbeb3a1
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-14 12:08:47 +0530

    9. Shortest Toutes 2 done using Warshall floyd algo

--- 4. Graph Algorithms/9. Shortest Routes 2/main.cpp
+++ 4. Graph Algorithms/9. Shortest Routes 2/main.cpp
@@ -1,95 +1,66 @@
 // https://cses.fi/problemset/task/1671
 
-
 #include "bits/stdc++.h"
 #define int long long
 #define vi vector<int>
-#define vvi vector<vi >
+#define vvi vector<vi>
 #define vb vector<bool>
-#define vvb vector<vb >
-#define fr(i,n) for(int i=0; i<(n); i++)
-#define rep(i,a,n) for(int i=(a); i<=(n); i++)
-#define nl cout<<"\n"
-#define dbg(var) cout<<#var<<"="<<var<<" "
-#define all(v) v.begin(),v.end()
+#define vvb vector<vb>
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
 #define sz(v) (int)(v.size())
-#define srt(v)  sort(v.begin(),v.end())         // sort 
-#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
-#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
-#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-// make sure to sort before applying unique // else only consecutive duplicates would be removed 
-#define bin(x,y)  bitset<y>(x) 
+#define srt(v) sort(v.begin(), v.end())         
+#define mxe(v) *max_element(v.begin(), v.end()) 
+#define mne(v) *min_element(v.begin(), v.end()) 
+#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
 using namespace std;
-int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
-
-
-
-void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
-void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
-void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
-// ================================== take ip/op like vector,pairs directly!==================================
-template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
-template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
-template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
-// ===================================END Of the input module ==========================================
-
 
 const long long INF = 1e18;
 
 void solve() {
-    int n, m; 
-    cin >> n >> m;
-
-    vector<vector<pair<int,int>>> adj(n+1);
-    for(int i = 0; i < m; i++) {
+    int n, m, q;
+    cin >> n >> m >> q;
+    
+    vector<vector<long long>> dist(n + 1, vector<long long>(n + 1, INF));
+    
+    // distance to self is zero
+    for (int i = 1; i <= n; i++) dist[i][i] = 0;
+    
+    // input roads
+    for (int i = 0; i < m; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        adj[a].push_back({b, c}); 
+        dist[a][b] = min(dist[a][b], (long long)c);
+        dist[b][a] = min(dist[b][a], (long long)c); // two-way road
     }
-
-    int source = 1;
-    vector<long long> distance(n+1, INF);
-    distance[source] = 0;
-
-    priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<>> pq;
-    pq.push({0, source});
-
-    while(!pq.empty()) {
-        auto [dist_u, u] = pq.top();
-        pq.pop();
-
-        if(dist_u > distance[u]) continue;
-
-        for(auto [v, w] : adj[u]) {
-            if(distance[u] + w < distance[v]) {
-                distance[v] = distance[u] + w;
-                pq.push({distance[v], v});
+    
+    // Floyd-Warshall all-pairs shortest paths
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF)
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
     }
-
-    for(int i = 1; i <= n; i++) {
-        if()
-        cout << distance[i] << " ";
+    
+    // answer queries
+    while (q--) {
+        int a, b;
+        cin >> a >> b;
+        if (dist[a][b] == INF) cout << "-1\n";
+        else cout << dist[a][b] << "\n";
     }
-    cout << "\n";
 }
 
-
-int32_t main()
-{
- 
- ios_base::sync_with_stdio(false);
- cin.tie(NULL);
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int T = 1;
-    while (T--)
-    {
-        solve();
-    }
+    while (T--) solve();
     return 0;
 }
-
-    

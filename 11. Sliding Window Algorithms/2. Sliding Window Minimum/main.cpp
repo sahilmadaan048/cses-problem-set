19def768525c99c44709197da8106c7378e734f3
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-17 17:17:08 +0530

    2. Sliding Window Minimum

---
+++ 11. Sliding Window Algorithms/2. Sliding Window Minimum/main.cpp
@@ -0,0 +1,79 @@
+// https://cses.fi/problemset/task/3221
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+void solve(){
+    int n, k; 
+    cin >> n >> k;
+
+    int x, a, b, c; 
+    cin >> x >> a >> b >> c;
+
+    vector<int> temp(n);
+    temp[0] = x;
+    for(int i=1; i<n; i++) temp[i] = (a * temp[i-1] + b) % c;
+
+    int sum = 0;
+    for(int i=0; i<k; i++) sum += temp[i];
+
+    // int mini = sum;
+    int ans  = *min_element(temp.begin(), temp.begin() + k);
+
+    for(int i=1; i<=n-k; i++) {
+        sum += temp[i+k-1] - temp[i-1];
+        // mini = min(mini, sum);
+        ans ^= *min_element(temp.begin() + i, temp.begin() + i + k);
+    }
+
+    cout << ans << endl;  
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+

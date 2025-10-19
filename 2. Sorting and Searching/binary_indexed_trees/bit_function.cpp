cd4a0c596fad5d14bc771875e5836aa3a34eebf8
Sahil Madaan <madaan.sahil27@gmail.com>
2025-10-19 12:52:16 +0530

    BIT - luc bhai

---
+++ 2. Sorting and Searching/binary_indexed_trees/bit_function.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+int bit[N];
+
+void update(int i, int x) {
+    for(; i<n; i+=(i&-i)) {
+        bit[i] += x;
+    }
+}
+
+int sum(int i) {
+    int ans = 0;
+    for(; i>0; i-=(i&-i)) {
+        ans += bit[i];
+    }
+    return ans;
+}
+
+int main() {
+
+}

ef2708944d97d3dee57153fc1c8a82c8af6cce03
Sahil Madaan <madaan.sahil27@gmail.com>
2025-10-20 15:22:13 +0530

    segment trees - 2. Fenwick Tree

--- 18. Segment Trees/2. Fenwick Tree (rev)/main.cpp
+++ 18. Segment Trees/2. Fenwick Tree (rev)/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    // your code here
-    return 0;
+  int n;
+  cin >> n;
+  vector<int> temp(n);
+  for (int i = 0; i < n; i++)
+    cin >> temp[i];
+  for (auto ele : temp)
+    cout << ele << " ";
+
+  cout << endl;
 }

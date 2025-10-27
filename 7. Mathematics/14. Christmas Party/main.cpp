5ba4184ab2180ae93fadca8607cc2bf67118c17e
Sahil Madaan <madaan.sahil27@gmail.com>
2025-10-28 03:52:36 +0530

    MATHS - 14. Christmas Party

---
+++ 7. Mathematics/14. Christmas Party/main.cpp
@@ -0,0 +1,69 @@
+// https://cses.fi/problemset/task/1717
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+int32_t main()
+{
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long n;
+    cin >> n;
+    
+    if (n == 1) {
+        cout << 0;
+        return 0;
+    }
+    if (n == 2) {
+        cout << 1;
+        return 0;
+    }
+    
+    vector<long long> der(n + 1);
+    der[1] = 0;
+    der[2] = 1;
+    
+    for (int i = 3; i <= n; i++) {
+        der[i] = ( (i - 1) * ((der[i - 1] + der[i - 2]) % MOD) ) % MOD;
+    }
+    
+    cout << der[n] % MOD;
+    return 0;
+}
+
+    

d251e803df56e31bb22d2c2c72a884acfbdae897
Sahil Madaan <madaan.sahil27@gmail.com>
2025-11-06 13:49:43 +0530

    sorting and searching  - 28. Nearest smaller values

--- 2. Sorting and Searching/28. Nearest Smaller Values/main.cpp
+++ 2. Sorting and Searching/28. Nearest Smaller Values/main.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
+// https://cses.fi/problemset/task/1645
+ 
+#include<bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
-    // your code here
-    return 0;
+	int n; cin >> n;
+	vector<int> arr(n), result(n);
+	for(int i=0; i<n; i++) cin >> arr[i];
+	stack<int> st;
+ 
+	for(int i=0; i<n; i++) {
+		while(!st.empty() and arr[st.top()] >= arr[i]){
+			st.pop();
+		}
+ 
+		if(!st.empty()){
+			result[i] = st.top()+1;
+		}
+		st.push(i);
+	}
+ 
+	for(auto ele : result) cout << ele << " " ;
+ 
+	return 0 ;
+
 }
     

d30436529e50966cab355e75fc0f0454d72a1012
Sahil Madaan <madaan.sahil27@gmail.com>
2026-04-01 01:19:12 +0530

    4. Graph Algorithms 22. Road Reparation

---
+++ 4. Graph Algorithms/22. Road Reparation/main.cpp
@@ -0,0 +1,117 @@
+// https://cses.fi/problemset/task/1675
+
+// Author - sahilmadaan048
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+int n, m;
+
+struct DSU {
+    vector<int> parent, size;
+
+    DSU(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 1; i <= n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    bool unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a == b) return false;
+
+        if (size[a] < size[b])
+            swap(a, b);
+
+        parent[b] = a;
+        size[a] += size[b];
+        return true;
+    }
+};
+
+vector<tuple<int, int, int>> edges;
+
+void solve(){
+   cin >> n >> m;
+
+   for(int i=0; i<m; i++) {
+      int a, b, c;
+      cin >> a >> b >> c;
+      edges.push_back({c, a, b});
+   }
+
+   sort(edges.begin(), edges.end());
+
+   DSU dsu(n);
+
+   int total_cost = 0;
+   int edges_used = 0;
+
+   for(auto [c, a, b]: edges) {
+      if(dsu.unite(a, b)) {
+         total_cost += c;
+         edges_used++;
+      }
+   }
+
+   if(edges_used != n-1) {
+      cout << "IMPOSSIBLE\n";
+   }else {
+      cout << total_cost << "\n";
+   }
+
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}

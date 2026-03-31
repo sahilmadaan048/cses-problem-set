26162b2b0f26c326f8496de9c32bf5e5a9567728
Sahil Madaan <madaan.sahil27@gmail.com>
2026-03-31 23:52:26 +0530

    4. Graph Algorithms 19. Planets Queries I

---
+++ 4. Graph Algorithms/19. Planets Queries I/main.cpp
@@ -0,0 +1,38 @@
+// https://cses.fi/problemset/task/1750/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 2e5 + 5;
+const int MAXD = 30;  
+int n, q;
+
+int parent[MAXN][MAXD];
+
+int jump(int a, int d) {
+    for (int i = 0; i < MAXD; i++)
+        if (d & (1 << i)) a = parent[a][i];
+    return a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) { 
+        cin >> parent[i][0]; 
+    }
+    for (int d = 1; d < MAXD; d++) {
+        for (int i = 1; i <= n; i++) { 
+            parent[i][d] = parent[parent[i][d - 1]][d - 1]; 
+        }
+    }
+
+    while (q--) {
+        int a, d;
+        cin >> a >> d;
+        cout << jump(a, d) << '\n';
+    }
+}

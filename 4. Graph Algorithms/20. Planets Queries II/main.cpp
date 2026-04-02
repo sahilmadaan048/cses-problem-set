334d8ec69187e53d34a86182cca17d9020c01cdc
Sahil Madaan <madaan.sahil27@gmail.com>
2026-04-02 20:17:05 +0530

    changes

--- 4. Graph Algorithms/20. Planets Queries II/main.cpp
+++ 4. Graph Algorithms/20. Planets Queries II/main.cpp
@@ -1,4 +1,4 @@
-// https://cses.fi/problemset/task/1675
+// https://cses.fi/problemset/task/1160
 
 // Author - sahilmadaan048
 
@@ -36,70 +36,18 @@ template<typename typC,typename typD> ostream &operator<<(ostream &cout,const ve
 template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
 // ===================================END Of the input module ==========================================
 
-int n, m;
-
-struct DSU {
-    vector<int> parent, size;
-
-    DSU(int n) {
-        parent.resize(n + 1);
-        size.resize(n + 1, 1);
-        for (int i = 1; i <= n; i++)
-            parent[i] = i;
-    }
-
-    int find(int x) {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    bool unite(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if (a == b) return false;
-
-        if (size[a] < size[b])
-            swap(a, b);
-
-        parent[b] = a;
-        size[a] += size[b];
-        return true;
-    }
-};
-
-vector<tuple<int, int, int>> edges;
-
 void solve(){
-   cin >> n >> m;
+   int n, q; cin >> n >> q;
 
-   for(int i=0; i<m; i++) {
-      int a, b, c;
-      cin >> a >> b >> c;
-      edges.push_back({c, a, b});
+   vector<int> a(n);
+
+   for(int i=0; i<n; i++) {
+      cin >> a[i];
    }
 
-   sort(edges.begin(), edges.end());
-
-   DSU dsu(n);
-
-   int total_cost = 0;
-   int edges_used = 0;
-
-   for(auto [c, a, b]: edges) {
-      if(dsu.unite(a, b)) {
-         total_cost += c;
-         edges_used++;
-      }
+   while(q--) {
+        
    }
-
-   if(edges_used != n-1) {
-      cout << "IMPOSSIBLE\n";
-   }else {
-      cout << total_cost << "\n";
-   }
-   return;
 }
 
 int32_t main()

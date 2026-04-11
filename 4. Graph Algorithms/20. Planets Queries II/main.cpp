621232fb79bde43fe66e498e3daac5e31988e909
Sahil Madaan <madaan.sahil27@gmail.com>
2026-04-11 23:20:22 +0530

    4. Graph Algorithms 20. Planets Queuries II

--- 4. Graph Algorithms/20. Planets Queries II/main.cpp
+++ 4. Graph Algorithms/20. Planets Queries II/main.cpp
@@ -2,65 +2,112 @@
 
 // Author - sahilmadaan048
 
-#include "bits/stdc++.h"
-#define int long long
-#define uint unsigned long long
-#define vi vector<int>
-#define vvi vector<vi >
-#define vb vector<bool>
-#define vvb vector<vb >
-#define fr(i,n) for(int i=0; i<(n); i++)
-#define rep(i,a,n) for(int i=(a); i<=(n); i++)
-#define nl cout<<"\n"
-#define dbg(var) cout<<#var<<"="<<var<<" "
-#define all(v) v.begin(),v.end()
-#define sz(v) (int)(v.size())
-#define srt(v)  sort(v.begin(),v.end())         // sort 
-#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
-#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
-#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-// make sure to sort before applying unique // else only consecutive duplicates would be removed 
-#define bin(x,y)  bitset<y>(x) 
+
+#include <bits/stdc++.h>
 using namespace std;
-int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
 
+const int MAXN = 200005;
+const int LOG = 20;
 
-void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
-void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
-void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
-// ================================== take ip/op like vector,pairs directly!==================================
-template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
-template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
-template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
-// ===================================END Of the input module ==========================================
+int n, q;
+int nxt[MAXN];
+int up[MAXN][LOG];
+int depth[MAXN];
+int comp[MAXN];
+int cycle_id[MAXN];
+int cycle_pos[MAXN];
+int cycle_size[MAXN];
+bool vis[MAXN];
+bool in_stack[MAXN];
 
-void solve(){
-   int n, q; cin >> n >> q;
+void dfs(int u) {
+   vis[u] = true;
+   in_stack[u] = true;
+   int v = nxt[u];
 
-   vector<int> a(n);
+   if(!vis[v]) {
+      dfs(v);
+   } else if(in_stack[v]) {
+      // found cycle
 
-   for(int i=0; i<n; i++) {
-      cin >> a[i];
+      int cur = v;
+      int cid = v;
+      int pos = 0;
+      do {
+         cycle_id[cur] = cid;
+         cycle_pos[cur] = pos++;
+         cur = nxt[cur];
+      } while(cur != v);
+      cycle_size[cid] = pos;
    }
 
-   while(q--) {
-        int a, b; cin >> a >> b;
-        cout << a << " " << b << endl;
+   in_stack[u] = false;
+
+   if(!cycle_id[u]) {
+      cycle_id[u] = cycle_id[nxt[u]];
+      depth[u] = depth[nxt[u]] + 1;
    }
 }
 
-int32_t main()
-{
- 
- ios_base::sync_with_stdio(false);
- cin.tie(NULL);
+int jump(int u, int k) {
+   for(int i=0; i<LOG; i++) {
+      if((k & (1 << i))) {
+         u = up[u][i];
+      }
+   }
+   return u;
+}
 
-    int T = 1;
-    while (T--)
-    {
-        solve();
-    }
-    return 0;
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   cin >> n >> q;
+
+   for(int i=1; i<=n; i++) {
+      cin >> nxt[i];
+   }
+
+   for(int i=1; i<=n; i++) {
+      up[i][0] = nxt[i];
+   }
+
+   for(int j=1; j<LOG; j++) {
+      for(int i=1; i<=n; i++) {
+         up[i][j] = up[up[i][j-1]][j-1];
+      }
+   }
+
+   for(int i=1; i<=n; i++) {
+      if(!vis[i]) {
+         dfs(i);
+      }
+   }
+
+   while(q--) {
+      int a, b; cin >> a >> b;
+
+      if(cycle_id[a] != cycle_id[b]) {
+         cout << -1 << "\n";
+         continue;
+      }
+
+      if(depth[a] >= depth[b]) {
+         int diff = depth[a] - depth[b];
+         if(jump(a, diff) == b) {
+            cout << diff << "\n";
+            continue;
+         }
+      }
+
+      if (depth[b] == 0) {
+         int dist = depth[a];
+         int entry = jump(a, depth[a]);
+         int cid = cycle_id[a];
+         int cyc_dist = (cycle_pos[b] - cycle_pos[entry] + cycle_size[cid]) % cycle_size[cid];
+         cout << dist + cyc_dist << "\n";
+      } else {
+         cout << -1 << "\n";
+      }
+   }
 }

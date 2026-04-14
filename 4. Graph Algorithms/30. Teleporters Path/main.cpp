d592e659deee55d570073a75fbc9cd5bc04f61a5
Sahil Madaan <madaan.sahil27@gmail.com>
2026-04-14 20:12:36 +0530

    4. Graph Algorithms 30. Teleporters Path

---
+++ 4. Graph Algorithms/30. Teleporters Path/main.cpp
@@ -0,0 +1,109 @@
+// https://cses.fi/problemset/task/1693
+
+// Author - sahilmadaan048
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+void solve(){
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n);
+    vector<int> indeg(n, 0), outdeg(n, 0);
+
+    for(int i = 0; i < m; i++){
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        adj[a].push_back(b);
+        outdeg[a]++;
+        indeg[b]++;
+    }
+
+    if(outdeg[0] != indeg[0] + 1 || indeg[n-1] != outdeg[n-1] + 1){
+        cout << "IMPOSSIBLE\n";
+        return;
+    }
+
+    for(int i = 1; i < n-1; i++){
+        if(indeg[i] != outdeg[i]){
+            cout << "IMPOSSIBLE\n";
+            return;
+        }
+    }
+
+    // Hierholzer's algorithm
+    stack<int> st;
+    vector<int> path;
+    st.push(0);
+
+    while(!st.empty()){
+        int node = st.top();
+
+        if(!adj[node].empty()){
+            int next = adj[node].back();
+            adj[node].pop_back();
+            st.push(next);
+        }
+        else{
+            path.push_back(node);
+            st.pop();
+        }
+    }
+
+    if(path.size() != m+1){
+        cout << "IMPOSSIBLE\n";
+        return;
+    }
+
+    reverse(path.begin(), path.end());
+
+    for(int x : path)
+        cout << x+1 << " ";
+    cout << "\n";
+}
+
+int32_t main()
+{
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T = 1;
+  while (T--)
+  {
+    solve();
+ }
+ return 0;
+}

1dad43c560f6ffdbbaeade2e6949cae383bb7f14
Sahil Madaan <madaan.sahil27@gmail.com>
2026-04-19 01:29:30 +0530

    9. Counting Paths

--- 6. Tree Algorithms/9. Counting Paths/main.cpp
+++ 6. Tree Algorithms/9. Counting Paths/main.cpp
@@ -37,21 +37,112 @@ template<typename typC,typename typD> ostream &operator<<(ostream &cout,const ve
 template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
 // ===================================END Of the input module ==========================================
 
+const int MAXN = 2e5+5;
+const int LOG = 28;
+
+vector<int> g[MAXN];
+int parent[LOG][MAXN];
+int depth[MAXN];
+int cnt[MAXN];
+int n, m;
+
+void dfs(int node, int par, int d) {
+   parent[0][node] = par;
+   depth[node] = d;
+   for(auto child: g[node]) {
+      if(child == par) continue;
+      dfs(child, node, d+1);
+   }
+}
+
+int lca(int a, int b) {
+   if(depth[a] < depth[b]) {
+      swap(a, b);
+   }
+
+   int diff = depth[a] - depth[b];
+   for(int i=0; i<LOG; i++) {
+      if(diff & (1 << i)) {
+         a = parent[i][a];
+      }
+   }
+
+   if(a == b) return a;
+
+   for(int i=LOG-1; i>=0; i--) {
+      if(parent[i][a] != parent[i][b]) {
+         a = parent[i][a];
+         b = parent[i][b];
+      }
+   }
+
+   return parent[0][a];
+}
+
+void dfs2(int node, int par) {
+   for(auto child: g[node]) {
+      if(child == par) continue;
+      dfs2(child, node);
+      cnt[node] += cnt[child];
+   }
+}
 
 void solve(){
+   cin >> n >> m;
 
+   for(int i=1; i<=n; i++) {
+      g[i].clear();
+   }
+
+   for(int i=1; i<=n-1; i++) {
+      int a, b; cin >> a >> b;
+      g[a].push_back(b);
+      g[b].push_back(a);
+   }
+
+   dfs(1, 0, 0);
+
+   for(int i=1; i<LOG; i++) {
+      for(int j=1; j<=n; j++) {
+         parent[i][j] = parent[i-1][parent[i-1][j]];
+      }
+   }
+
+
+   while(m--) {
+      int a, b; cin >> a >> b;
+
+      int l = lca(a, b);
+
+      cnt[a]++;
+      cnt[b]++;
+
+      cnt[l]--;
+
+      if(parent[0][l] != 0) {
+         cnt[parent[0][l]]--;
+      }
+   }
+
+   dfs2(1, 0);
+
+   for(int i=1; i<=n; i++) {
+      cout << cnt[i] << " ";
+   }
+
+   cout << "\n";
 }
 
 int32_t main()
 {
- 
- ios_base::sync_with_stdio(false);
- cin.tie(NULL);
 
-    int T = 1;
-    while (T--)
-    {
-        solve();
-    }
-    return 0;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T = 1;
+  while (T--)
+  {
+    solve();
+ }
+ return 0;
 }

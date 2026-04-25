9c95fc324969653bb8ab8b8526fc9cdcfa363ced
Sahil Madaan <madaan.sahil27@gmail.com>
2026-04-25 15:01:59 +0530

    14. Findinig a centroid

---
+++ 6. Tree Algorithms/14. Finding a Centroid/main.cpp
@@ -0,0 +1,93 @@
+// https://cses.fi/problemset/task/2079
+
+// Author - sahilmadaan048
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+const int MAXN = 200005;
+
+vector<int> adj[MAXN];
+int subtree[MAXN];
+int n;
+
+void dfs(int u, int parent) {
+    subtree[u] = 1;
+
+    for (int v : adj[u]) {
+        if (v == parent) continue;
+
+        dfs(v, u);
+        subtree[u] += subtree[v];
+    }
+}
+
+int find_centroid(int u, int parent) {
+    for (int v : adj[u]) {
+        if (v == parent) continue;
+
+        if (subtree[v] > n / 2)
+            return find_centroid(v, u);
+    }
+
+    return u;
+}
+
+void solve() {
+        cin >> n;
+
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    dfs(1, 0);
+
+    cout << find_centroid(1, 0) << '\n';
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t=1;
+
+  while(t--) {
+     solve();
+ }
+}

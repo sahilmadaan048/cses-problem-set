e5fafbe5cd84d3ca896e415142fe08b91a45ff0a
Sahil Madaan <madaan.sahil27@gmail.com>
2026-04-29 01:27:57 +0530

    6. Longest Palindrome (Manacher implementation)

---
+++ 8. String Algorithms/6. Longest Palindrome/main2.cpp
@@ -0,0 +1,93 @@
+// https://cses.fi/problemset/task/1111
+// Author - sahilmadaan048
+// https://leetcode.com/problems/longest-palindromic-substring/solutions/6141600/video-using-two-pointers-python-javascri-gsz2/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+void solve() {
+    string s;
+    cin >> s;
+
+    string t = "#";
+    for (char c : s) {
+        t += c;
+        t += "#";
+    }
+
+    int n = t.size();
+    vector<int> p(n);
+
+    int center = 0, right = 0;
+    int max_len = 0, max_center = 0;
+
+    for (int i = 0; i < n; i++) {
+        int mirror = 2 * center - i;
+
+        if (i < right)
+            p[i] = min(right - i, p[mirror]);
+
+        while (i - p[i] - 1 >= 0 &&
+               i + p[i] + 1 < n &&
+               t[i - p[i] - 1] == t[i + p[i] + 1])
+            p[i]++;
+
+        if (i + p[i] > right) {
+            center = i;
+            right = i + p[i];
+        }
+
+        if (p[i] > max_len) {
+            max_len = p[i];
+            max_center = i;
+        }
+    }
+
+    int start = (max_center - max_len) / 2;
+    cout << s.substr(start, max_len) << "\n";
+}
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}

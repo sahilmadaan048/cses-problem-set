cddf7bf2775633ecd2f5584afc9e9238a155f93b
Sahil Madaan <madaan.sahil27@gmail.com>
2026-05-19 12:07:32 +0530

    1. Subuordinates

---
+++ 6. Tree Algorithms/1. Subordinates/main.cpp
@@ -0,0 +1,85 @@
+// https://cses.fi/problemset/task/1674
+
+// Author - sahilmadaan048
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+vector<vector<int>> graph;
+vector<int> subtree_size;
+
+int dfs(int node) {
+	int size = 0;
+	for(auto child: graph[node]) {
+		size += 1+dfs(child);
+	}
+	subtree_size[node] = size;
+	return size;
+}
+
+void solve(){
+	int n; cin >> n ;
+	vector<int> temp(n+1);
+
+	graph.resize(n+1);
+	subtree_size.resize(n+1, 0);
+
+	for(int i=2; i<=n; i++) {
+		cin >> temp[i];
+
+		 // create the graph where "temp[i]" is the parent of i  
+		graph[temp[i]].push_back(i);
+	}
+
+	dfs(1);
+
+	for(int i=1; i<=n; i++) {
+		cout << subtree_size[i] << " ";
+	}
+	return;
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}

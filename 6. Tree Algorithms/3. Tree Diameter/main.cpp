db1e14580a5f1270fc13c8675d3443c8c7a71049
Sahil Madaan <madaan.sahil27@gmail.com>
2026-05-19 12:36:38 +0530

    3. Tree Diameter

---
+++ 6. Tree Algorithms/3. Tree Diameter/main.cpp
@@ -0,0 +1,96 @@
+// https://cses.fi/problemset/task/1131
+// Author - sahilmadaan048
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+vector<vector<int>> graph;
+
+int farthestNode;
+int maxDist;
+
+void dfs(int node, int parent, int dist) {	
+	if(dist > maxDist) {
+		maxDist = dist;
+		farthestNode = node;
+	}
+
+	for(auto child: graph[node]) {
+		if(child == parent) continue;
+
+		dfs(child, node, dist+1);
+	}
+}
+
+void solve(){
+	int n; cin >> n;
+
+	graph.resize(n+1);
+
+	for(int i=1; i<n; i++) {
+		int a, b; cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+
+
+	// lets do the first dfs
+	maxDist = -1;
+	dfs(1, 0, 0);
+
+
+	int start = farthestNode;
+
+	// second dfs
+	maxDist = -1;
+	dfs(start, 0, 0);
+
+	// cout << start << endl;
+
+	cout << maxDist << "\n";
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}

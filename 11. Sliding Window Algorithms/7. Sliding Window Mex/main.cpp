27b2430c81d9b8eba12aef61d961a9582f62986b
Sahil Madaan <madaan.sahil27@gmail.com>
2026-08-14 14:22:41 +0530

    7. Sliding Window Mex

---
+++ 11. Sliding Window Algorithms/7. Sliding Window Mex/main.cpp
@@ -0,0 +1,105 @@
+// https://cses.fi/problemset/task/3219
+
+// Author - sahilmadaan048
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+const int MAXN = 200005;
+
+map<int, int> frequency;
+set<int> missing_numbers;
+
+void init() {
+    for (int i = 0; i < MAXN; i++)
+        missing_numbers.insert(i);
+}
+
+void add(int val) {
+    if(val >= MAXN) return;
+    ++frequency[val];
+    missing_numbers.erase(val);
+}
+
+void remove(int val) {
+    if(val >= MAXN) return;
+    if(--frequency[val] == 0) {
+        missing_numbers.insert(val);
+    }
+}
+
+int get() {
+    return *missing_numbers.begin();
+}
+
+void solve(){
+    int n, k; cin >> n >> k;
+    vector<int> a(n);
+
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+
+    init();
+
+    for(int i=0; i<k; i++) {
+        add(a[i]);
+    }
+
+    cout << get() << " ";
+
+    for(int i=k; i<n; i++) {
+        remove(a[i-k]);
+        add(a[i]);
+
+        cout << get() << " ";
+    }
+
+    cout << endl;
+
+}   
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
